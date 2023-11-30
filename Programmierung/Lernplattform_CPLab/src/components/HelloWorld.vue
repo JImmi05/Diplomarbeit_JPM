b8955d958fc4d7750ac53e17ee44e7a5c743bd88
Skoerdeman <magnus.adametz@student.htldornbirn.at>
2023-11-30 12:41:11 +0100

    Ersten Schritte (Startseite erstellt)
    Startseite erstellt

--- Programmierung/Lernplattform_CPLab/src/components/HelloWorld.vue
+++ Programmierung/Lernplattform_CPLab/src/components/HelloWorld.vue
@@ -1,44 +1,288 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
-</script>
-
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+  <div class="startseite">
+    <div class="div">
+      <p class="hands-on-ist-die">
+        Hands on“ ist die Divise bei den Lern und Übungsstationen. So lassen sich zum Beispiel mit dem neuen
+        „Cyber Physical Laboratory“ im Wifi Dornbirn unterschiedlichste industrielle Aufgabenstellungen und Szenarien
+        live erleben – vom einzelnen Transferband bis hin zur kompletten Produktionsanlage mit verschiedenen Modulen
+        und Arbeitsplätzen. Alles ist dabei hochgradig vernetzt, um digitale Skills, wie Programmierung und
+        Kommunikation quer durch alle Unternehmensbereiche zu erlernen, denn diese sind für die Fertigung der Zukunft
+        unumgänglich. Unsere Arbeit soll das Erlernen von zukunftsweisenden Automatisier
+        <br />
+        <br />
+        Modulares Lernsystem für praxisnahe Aus – und Weiterbildung, welche aufbauend auf klassischen Inhalten der
+        Elektrotechnik Kompetenzen neuer Technologien und Systeme in Kontext von Industrie 4.0 vermittelt.
+        Lernanleitung (Webseite, Videos, AR, …) in Form von Infos, aufbauenden Aufgaben und Anleitungen.
+      </p>
+      <div class="unser-ziel">UNSER ZIEL:</div>
+      <p class="text-wrapper">
+        Kleinförderbänder von Vetter vereinen Individualität mit maximaler Qualität. Sie sind kompakt, ohne
+        Störkonturen und überzeugen mit geschickt integrierten, leistungsstarken Motoren.
+      </p>
+      <img class="image" alt="Image" src="../assets/image-9.png" />
+      <p class="p">
+        Die einzelnen Funktionsmodule werden einfach auf die Bänder aufgesetzt und über Ein- und Ausgänge oder Profi
+        net/Profi - bus mit der integrierten Steuerung am Bandsystem verbunden. Damit sind sie innerhalb kürzester
+        Zeit einsatzbereit.
+      </p>
+      <img class="img" alt="Image" src="../assets/logo.svg" />
+      <button class="button">
+        <div class="text-wrapper-2">Erste Schritte</div>
+      </button>
+      <p class="text-wrapper-3">Lernsystem der digitalen Lernfabrik im WIFI Dornbirn</p>
+      <header class="header">
+        <div class="cart-button">
+          <div class="text-wrapper-4">Abmelden</div>
+        </div>
+        <div class="text-wrapper-5">Mein Profil</div>
+        <div class="text-wrapper-6">Aufträge</div>
+        <div class="text-wrapper-7">Anleitungen</div>
+        <div class="text-wrapper-8">Startseite</div>
+        <div class="text-wrapper-9">Name</div>
+      </header>
+    </div>
   </div>
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
+<script>
+export default {
+  name: "Startseite",
+};
+</script>
+
+<style>
+.startseite {
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  width: 100%;
+}
+
+.startseite .div {
+  background-color: #ffffff;
+  height: 2092px;
   position: relative;
-  top: -10px;
+  width: 1440px;
 }
 
-h3 {
-  font-size: 1.2rem;
+.startseite .hands-on-ist-die {
+  color: #000000;
+  font-family: "Inter-Regular", Helvetica;
+  font-size: 20px;
+  font-weight: 400;
+  left: 309px;
+  letter-spacing: 0;
+  line-height: 32px;
+  position: absolute;
+  top: 1554px;
+  width: 822px;
 }
 
-.greetings h1,
-.greetings h3 {
+.startseite .unser-ziel {
+  color: #000000;
+  font-family: "Inter-SemiBold", Helvetica;
+  font-size: 24px;
+  font-weight: 600;
+  left: 96px;
+  letter-spacing: 0.96px;
+  line-height: 38.4px;
+  position: absolute;
+  top: 1554px;
+  width: 181px;
+}
+
+.startseite .text-wrapper {
+  color: #000000;
+  font-family: "Inter-Medium", Helvetica;
+  font-size: 14px;
+  font-weight: 500;
+  left: 629px;
+  letter-spacing: -0.14px;
+  line-height: 22.4px;
+  position: absolute;
+  top: 1286px;
+  width: 786px;
+}
+
+.startseite .image {
+  height: 512px;
+  left: 695px;
+  object-fit: cover;
+  position: absolute;
+  top: 760px;
+  width: 666px;
+}
+
+.startseite .p {
+  color: #000000;
+  font-family: "Inter-Medium", Helvetica;
+  font-size: 14px;
+  font-weight: 500;
+  left: 144px;
+  letter-spacing: -0.14px;
+  line-height: 22.4px;
+  position: absolute;
+  top: 1418px;
+  width: 1029px;
+}
+
+.startseite .img {
+  height: 708px;
+  left: 144px;
+  object-fit: cover;
+  position: absolute;
+  top: 677px;
+  width: 453px;
+}
+
+.startseite .button {
+  all: unset;
+  background-color: #426b1f;
+  border-radius: 8px;
+  box-sizing: border-box;
+  height: 64px;
+  left: 607px;
+  overflow: hidden;
+  position: absolute;
+  top: 481px;
+  width: 227px;
+}
+
+.startseite .text-wrapper-2 {
+  color: #ffffff;
+  font-family: "Inter-SemiBold", Helvetica;
+  font-size: 20px;
+  font-weight: 600;
+  height: 26px;
+  left: 47px;
+  letter-spacing: 0;
+  line-height: 26px;
+  position: absolute;
+  top: 18px;
+  white-space: nowrap;
+}
+
+.startseite .text-wrapper-3 {
+  color: #000000;
+  font-family: "Newsreader-Regular", Helvetica;
+  font-size: 64px;
+  font-weight: 400;
+  left: 217px;
+  letter-spacing: -1.28px;
+  line-height: 76.8px;
+  position: absolute;
   text-align: center;
+  top: 278px;
+  width: 1020px;
 }
 
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
+.startseite .header {
+  background-color: transparent;
+  height: 112px;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 1440px;
+}
+
+.startseite .cart-button {
+  background-color: #426b1f;
+  border-radius: 8px;
+  height: 48px;
+  left: 1218px;
+  overflow: hidden;
+  position: absolute;
+  top: 32px;
+  width: 126px;
+}
+
+.startseite .text-wrapper-4 {
+  color: #ffffff;
+  font-family: "Inter-SemiBold", Helvetica;
+  font-size: 16px;
+  font-weight: 600;
+  height: 21px;
+  left: 24px;
+  letter-spacing: 0;
+  line-height: 20.8px;
+  position: absolute;
+  text-align: center;
+  top: 12px;
+  white-space: nowrap;
+}
+
+.startseite .text-wrapper-5 {
+  color: #000000;
+  font-family: "Inter-Regular", Helvetica;
+  font-size: 16px;
+  font-weight: 400;
+  height: 21px;
+  left: 1107px;
+  letter-spacing: 0;
+  line-height: 20.8px;
+  position: absolute;
+  text-align: center;
+  top: 44px;
+  white-space: nowrap;
+}
+
+.startseite .text-wrapper-6 {
+  color: #000000;
+  font-family: "Inter-Regular", Helvetica;
+  font-size: 16px;
+  font-weight: 400;
+  height: 20px;
+  left: 1003px;
+  letter-spacing: 0;
+  line-height: 20.8px;
+  position: absolute;
+  text-align: center;
+  top: 45px;
+  white-space: nowrap;
+  width: 73px;
+}
+
+.startseite .text-wrapper-7 {
+  color: #000000;
+  font-family: "Inter-Regular", Helvetica;
+  font-size: 16px;
+  font-weight: 400;
+  height: 21px;
+  left: 882px;
+  letter-spacing: 0;
+  line-height: 20.8px;
+  position: absolute;
+  text-align: center;
+  top: 45px;
+  white-space: nowrap;
+}
+
+.startseite .text-wrapper-8 {
+  color: #000000;
+  font-family: "Inter-Regular", Helvetica;
+  font-size: 16px;
+  font-weight: 400;
+  height: 21px;
+  left: 777px;
+  letter-spacing: 0;
+  line-height: 20.8px;
+  position: absolute;
+  text-align: center;
+  top: 44px;
+  white-space: nowrap;
+}
+
+.startseite .text-wrapper-9 {
+  color: #426b1f;
+  font-family: "Newsreader-Medium", Helvetica;
+  font-size: 32px;
+  font-weight: 500;
+  left: 96px;
+  letter-spacing: -0.32px;
+  line-height: 32px;
+  position: absolute;
+  top: 40px;
+  white-space: nowrap;
 }
 </style>
--- Programmierung/Lernplattform_CPLab/src/components/WelcomeItem.vue
+++ Programmierung/Lernplattform_CPLab/src/components/WelcomeItem.vue
@@ -1,87 +1,511 @@
 <template>
-  <div class="item">
-    <i>
-      <slot name="icon"></slot>
-    </i>
-    <div class="details">
-      <h3>
-        <slot name="heading"></slot>
-      </h3>
-      <slot></slot>
+  <div class="aufgaben-seite">
+    <div class="div">
+      <div class="summary">
+        <button class="button">
+          <img class="icon" alt="Icon" src="icon.svg" />
+          <div class="text-wrapper">Abgeben</div>
+        </button>
+        <div class="text-wrapper-2">5.5h</div>
+        <div class="text-wrapper-3">Gesamt</div>
+        <div class="text-wrapper-4">3h</div>
+        <div class="text-wrapper-5">Bohrstation</div>
+        <div class="text-wrapper-6">1h</div>
+        <div class="text-wrapper-7">Sensoren des Förderbands</div>
+        <div class="text-wrapper-8">1.5h</div>
+        <div class="text-wrapper-9">Förderband</div>
+        <div class="frame">
+          <div class="text-wrapper-10">Aufträge</div>
+        </div>
+      </div>
+      <div class="aufgabe">
+        <div class="text-wrapper-11">1h</div>
+        <div class="text-wrapper-12">Datum Abgabe</div>
+        <div class="text-wrapper-13">Sensoren des Förderbands</div>
+        <img class="img" alt="Noonbrew ekmak" src="noonbrew-zicb4-ekmak-unsplash.png" />
+      </div>
+      <div class="aufgabe-2">
+        <div class="text-wrapper-11">3h</div>
+        <div class="text-wrapper-12">Datum Abgabe</div>
+        <div class="text-wrapper-13">Bohrstation</div>
+        <img class="img" alt="Fkphulv m" src="k8-0-fkphulv-m-unsplash-2.png" />
+      </div>
+      <div class="aufgabe-3">
+        <div class="text-wrapper-11">1.5 h</div>
+        <div class="text-wrapper-12">Datum Abgabe</div>
+        <div class="text-wrapper-13">Förderband</div>
+        <img class="img" alt="Ed o neil" src="ed-o-neil-avvdzlhdowa-unsplash-1.png" />
+      </div>
+      <div class="page-heading">
+        <img class="divider" alt="Divider" src="divider.svg" />
+        <div class="frame-wrapper">
+          <div class="div-wrapper">
+            <div class="div-wrapper">
+              <div class="text-wrapper-14">Aufträge</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="navigation">
+        <div class="cart-button">
+          <div class="text-wrapper-15">Abmelden</div>
+        </div>
+        <div class="text-wrapper-16">Mein Profil</div>
+        <div class="text-wrapper-17">Aufträge</div>
+        <div class="text-wrapper-18">Anleitungen</div>
+        <div class="text-wrapper-19">Startseite</div>
+        <div class="text-wrapper-20">Name</div>
+      </div>
     </div>
   </div>
 </template>
 
-<style scoped>
-.item {
-  margin-top: 2rem;
+<script>
+export default {
+  name: "AufgabenSeite",
+};
+</script>
+
+<style>
+.aufgaben-seite {
+  background-color: #ffffff;
   display: flex;
+  flex-direction: row;
+  justify-content: center;
+  width: 100%;
+}
+
+.aufgaben-seite .div {
+  background-color: #ffffff;
+  height: 1024px;
+  position: relative;
+  width: 1440px;
+}
+
+.aufgaben-seite .summary {
+  background-color: #fafaf5;
+  border: 2px solid;
+  border-color: #e6e6e6;
+  border-radius: 24px;
+  height: 318px;
+  left: 949px;
+  overflow: hidden;
+  position: absolute;
+  top: 301px;
+  width: 395px;
+}
+
+.aufgaben-seite .button {
+  all: unset;
+  background-color: #426b1f;
+  border-radius: 8px;
+  box-sizing: border-box;
+  height: 48px;
+  left: 24px;
+  overflow: hidden;
+  position: absolute;
+  top: 246px;
+  width: 347px;
+}
+
+.aufgaben-seite .icon {
+  height: 32px;
+  left: 299px;
+  position: absolute;
+  top: 8px;
+  width: 32px;
+}
+
+.aufgaben-seite .text-wrapper {
+  color: #ffffff;
+  font-family: "Inter-SemiBold", Helvetica;
+  font-size: 16px;
+  font-weight: 600;
+  height: 21px;
+  left: 24px;
+  letter-spacing: 0;
+  line-height: 20.8px;
+  position: absolute;
+  top: 13px;
+  white-space: nowrap;
+}
+
+.aufgaben-seite .text-wrapper-2 {
+  color: #000000;
+  font-family: "Inter-SemiBold", Helvetica;
+  font-size: 16px;
+  font-weight: 600;
+  left: 336px;
+  letter-spacing: 0;
+  line-height: 20.8px;
+  position: absolute;
+  text-align: right;
+  top: 188px;
+  white-space: nowrap;
+}
+
+.aufgaben-seite .text-wrapper-3 {
+  color: #000000;
+  font-family: "Inter-SemiBold", Helvetica;
+  font-size: 16px;
+  font-weight: 600;
+  left: 24px;
+  letter-spacing: 0;
+  line-height: 20.8px;
+  position: absolute;
+  top: 191px;
+  white-space: nowrap;
+}
+
+.aufgaben-seite .text-wrapper-4 {
+  color: #000000;
+  font-family: "Inter-Regular", Helvetica;
+  font-size: 16px;
+  font-weight: 400;
+  left: 351px;
+  letter-spacing: 0;
+  line-height: 20.8px;
+  position: absolute;
+  text-align: right;
+  top: 154px;
+  white-space: nowrap;
+}
+
+.aufgaben-seite .text-wrapper-5 {
+  color: #000000;
+  font-family: "Inter-Regular", Helvetica;
+  font-size: 16px;
+  font-weight: 400;
+  left: 24px;
+  letter-spacing: 0;
+  line-height: 20.8px;
+  position: absolute;
+  top: 154px;
+  white-space: nowrap;
+}
+
+.aufgaben-seite .text-wrapper-6 {
+  color: #000000;
+  font-family: "Inter-Regular", Helvetica;
+  font-size: 16px;
+  font-weight: 400;
+  left: 354px;
+  letter-spacing: 0;
+  line-height: 20.8px;
+  position: absolute;
+  text-align: right;
+  top: 117px;
+  white-space: nowrap;
+}
+
+.aufgaben-seite .text-wrapper-7 {
+  color: #000000;
+  font-family: "Inter-Regular", Helvetica;
+  font-size: 16px;
+  font-weight: 400;
+  left: 24px;
+  letter-spacing: 0;
+  line-height: 20.8px;
+  position: absolute;
+  top: 117px;
+  white-space: nowrap;
+}
+
+.aufgaben-seite .text-wrapper-8 {
+  color: #000000;
+  font-family: "Inter-Regular", Helvetica;
+  font-size: 16px;
+  font-weight: 400;
+  left: 340px;
+  letter-spacing: 0;
+  line-height: 20.8px;
+  position: absolute;
+  text-align: right;
+  top: 80px;
+  white-space: nowrap;
+}
+
+.aufgaben-seite .text-wrapper-9 {
+  color: #000000;
+  font-family: "Inter-Regular", Helvetica;
+  font-size: 16px;
+  font-weight: 400;
+  left: 24px;
+  letter-spacing: 0;
+  line-height: 20.8px;
+  position: absolute;
+  top: 80px;
+  white-space: nowrap;
+}
+
+.aufgaben-seite .frame {
+  align-items: center;
+  display: inline-flex;
+  gap: 10px;
+  justify-content: center;
+  left: 14px;
+  padding: 10px;
+  position: absolute;
+  top: 14px;
+}
+
+.aufgaben-seite .text-wrapper-10 {
+  color: #000000;
+  font-family: "Inter-SemiBold", Helvetica;
+  font-size: 20px;
+  font-weight: 600;
+  letter-spacing: 0;
+  line-height: 26px;
+  margin-top: -1px;
+  position: relative;
+  white-space: nowrap;
+  width: fit-content;
+}
+
+.aufgaben-seite .aufgabe {
+  background-color: #fafaf5;
+  border: 2px solid;
+  border-color: #e6e6e6;
+  border-radius: 24px;
+  height: 159px;
+  left: 96px;
+  overflow: hidden;
+  position: absolute;
+  top: 491px;
+  width: 821px;
+}
+
+.aufgaben-seite .text-wrapper-11 {
+  color: #426b1f;
+  font-family: "Inter-SemiBold", Helvetica;
+  font-size: 20px;
+  font-weight: 600;
+  left: 184px;
+  letter-spacing: 0;
+  line-height: 26px;
+  position: absolute;
+  top: 52px;
+  white-space: nowrap;
+}
+
+.aufgaben-seite .text-wrapper-12 {
+  color: #000000;
+  font-family: "Inter-SemiBold", Helvetica;
+  font-size: 20px;
+  font-weight: 600;
+  left: 652px;
+  letter-spacing: 0;
+  line-height: 26px;
+  position: absolute;
+  text-align: right;
+  top: 22px;
+  white-space: nowrap;
+}
+
+.aufgaben-seite .text-wrapper-13 {
+  color: #000000;
+  font-family: "Inter-SemiBold", Helvetica;
+  font-size: 20px;
+  font-weight: 600;
+  left: 184px;
+  letter-spacing: 0;
+  line-height: 26px;
+  position: absolute;
+  top: 22px;
+  white-space: nowrap;
+}
+
+.aufgaben-seite .img {
+  height: 159px;
+  left: 0;
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  width: 160px;
+}
+
+.aufgaben-seite .aufgabe-2 {
+  background-color: #fafaf5;
+  border: 2px solid;
+  border-color: #e6e6e6;
+  border-radius: 24px;
+  height: 159px;
+  left: 96px;
+  overflow: hidden;
+  position: absolute;
+  top: 681px;
+  width: 821px;
+}
+
+.aufgaben-seite .aufgabe-3 {
+  background-color: #fafaf5;
+  border: 2px solid;
+  border-color: #e6e6e6;
+  border-radius: 24px;
+  height: 159px;
+  left: 96px;
+  overflow: hidden;
+  position: absolute;
+  top: 301px;
+  width: 821px;
+}
+
+.aufgaben-seite .page-heading {
+  background-color: #ffffff;
+  height: 149px;
+  left: 0;
+  position: absolute;
+  top: 112px;
+  width: 1440px;
+}
+
+.aufgaben-seite .divider {
+  height: 2px;
+  left: 92px;
+  object-fit: cover;
+  position: absolute;
+  top: 147px;
+  width: 1256px;
+}
+
+.aufgaben-seite .frame-wrapper {
+  align-items: center;
+  display: inline-flex;
+  gap: 10px;
+  justify-content: center;
+  left: 86px;
+  padding: 10px;
+  position: absolute;
+  top: 46px;
+}
+
+.aufgaben-seite .div-wrapper {
+  align-items: center;
+  display: inline-flex;
+  flex: 0 0 auto;
+  gap: 10px;
+  justify-content: center;
   position: relative;
 }
 
-.details {
-  flex: 1;
-  margin-left: 1rem;
+.aufgaben-seite .text-wrapper-14 {
+  color: #000000;
+  font-family: "Newsreader-Regular", Helvetica;
+  font-size: 64px;
+  font-weight: 400;
+  letter-spacing: -1.28px;
+  line-height: 76.8px;
+  margin-top: -1px;
+  position: relative;
+  white-space: nowrap;
+  width: fit-content;
 }
 
-i {
-  display: flex;
-  place-items: center;
-  place-content: center;
-  width: 32px;
-  height: 32px;
-
-  color: var(--color-text);
+.aufgaben-seite .navigation {
+  height: 112px;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 1440px;
 }
 
-h3 {
-  font-size: 1.2rem;
+.aufgaben-seite .cart-button {
+  background-color: #426b1f;
+  border-radius: 8px;
+  height: 48px;
+  left: 1218px;
+  overflow: hidden;
+  position: absolute;
+  top: 32px;
+  width: 126px;
+}
+
+.aufgaben-seite .text-wrapper-15 {
+  color: #ffffff;
+  font-family: "Inter-SemiBold", Helvetica;
+  font-size: 16px;
+  font-weight: 600;
+  height: 21px;
+  left: 24px;
+  letter-spacing: 0;
+  line-height: 20.8px;
+  position: absolute;
+  text-align: center;
+  top: 12px;
+  white-space: nowrap;
+}
+
+.aufgaben-seite .text-wrapper-16 {
+  color: #000000;
+  font-family: "Inter-Regular", Helvetica;
+  font-size: 16px;
+  font-weight: 400;
+  height: 21px;
+  left: 1107px;
+  letter-spacing: 0;
+  line-height: 20.8px;
+  position: absolute;
+  text-align: center;
+  top: 44px;
+  white-space: nowrap;
+}
+
+.aufgaben-seite .text-wrapper-17 {
+  color: #000000;
+  font-family: "Inter-Regular", Helvetica;
+  font-size: 16px;
+  font-weight: 400;
+  height: 20px;
+  left: 1003px;
+  letter-spacing: 0;
+  line-height: 20.8px;
+  position: absolute;
+  text-align: center;
+  top: 45px;
+  white-space: nowrap;
+  width: 73px;
+}
+
+.aufgaben-seite .text-wrapper-18 {
+  color: #000000;
+  font-family: "Inter-Regular", Helvetica;
+  font-size: 16px;
+  font-weight: 400;
+  height: 21px;
+  left: 882px;
+  letter-spacing: 0;
+  line-height: 20.8px;
+  position: absolute;
+  text-align: center;
+  top: 45px;
+  white-space: nowrap;
+}
+
+.aufgaben-seite .text-wrapper-19 {
+  color: #000000;
+  font-family: "Inter-Regular", Helvetica;
+  font-size: 16px;
+  font-weight: 400;
+  height: 21px;
+  left: 777px;
+  letter-spacing: 0;
+  line-height: 20.8px;
+  position: absolute;
+  text-align: center;
+  top: 44px;
+  white-space: nowrap;
+}
+
+.aufgaben-seite .text-wrapper-20 {
+  color: #426b1f;
+  font-family: "Newsreader-Medium", Helvetica;
+  font-size: 32px;
   font-weight: 500;
-  margin-bottom: 0.4rem;
-  color: var(--color-heading);
-}
-
-@media (min-width: 1024px) {
-  .item {
-    margin-top: 0;
-    padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
-  }
-
-  i {
-    top: calc(50% - 25px);
-    left: -26px;
-    position: absolute;
-    border: 1px solid var(--color-border);
-    background: var(--color-background);
-    border-radius: 8px;
-    width: 50px;
-    height: 50px;
-  }
-
-  .item:before {
-    content: ' ';
-    border-left: 1px solid var(--color-border);
-    position: absolute;
-    left: 0;
-    bottom: calc(50% + 25px);
-    height: calc(50% - 25px);
-  }
-
-  .item:after {
-    content: ' ';
-    border-left: 1px solid var(--color-border);
-    position: absolute;
-    left: 0;
-    top: calc(50% + 25px);
-    height: calc(50% - 25px);
-  }
-
-  .item:first-of-type:before {
-    display: none;
-  }
-
-  .item:last-of-type:after {
-    display: none;
-  }
+  left: 91px;
+  letter-spacing: -0.32px;
+  line-height: 32px;
+  position: absolute;
+  top: 40px;
+  white-space: nowrap;
 }
 </style>
