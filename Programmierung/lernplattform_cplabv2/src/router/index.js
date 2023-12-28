aed9a44bc843f5e3c52281b9a18b0eeedb3cf9e5
Skoerdeman <magnus.adametz@student.htldornbirn.at>
2023-12-28 12:01:58 +0100

    Website v2 log out Versuch
    funktioniert nocht nicht handleSignOut not defined

--- Programmierung/lernplattform_cplabv2/src/router/index.js
+++ Programmierung/lernplattform_cplabv2/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, getAuth } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -6,8 +6,27 @@ const router = createRouter({
     { path: "/", component: () => import("../views/Home.vue")},
     { path: "/register", component: () => import("../views/RegisterForm.vue")},
     { path: "/sign-in", component: () => import("../views/SignIn.vue")},
-    { path: "/feed", component: () => import("../views/Feed.vue")},
+    { 
+      path: "/feed", 
+      component: () => import("../views/Feed.vue"),
+      meta: {
+        requiresAuth: true,
+      },
+    },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((recorded) => recorded.meta.requiresAuth)) {
+    if (getAuth().currentUser) {
+      next();
+    } else {
+      alert("you dont have access!");
+      next("/");
+    }
+  } else {
+    next();
+  }
 });
 
 export default router
