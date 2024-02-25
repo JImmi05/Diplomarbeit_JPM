189cf48cbaba91b5e99ce6c31c585c1ba4a6bd76
masteroogway05 <piusstadler05@gmail.com>
2024-02-25 12:09:24 +0100

    kommentare
    kommentare backend hinzugefügt

--- Programmierung/lernplattform_cplabv3/src/store/index.js
+++ Programmierung/lernplattform_cplabv3/src/store/index.js
@@ -13,21 +13,24 @@ const store = createStore({   //Hier wird der Authentifizierungsspeicher angeleg
   state: {                    //state:  verfolgt den Status des Users
     user: null,               /*dieser wird am Anfang auf null gestzt. Das heißt der User 
                               ist nicht angemeldet*/
-    authIsReady: false        
+    authIsReady: false        //Bis wir wissen ob der User ein oder ausgeloggt ist die Authentifizierung nicht bereit
   },
-  mutations: {                    //mutations: wird verwendet um den user zu updaten
+  mutations: {                    //mutations: wird verwendet um etwas zu updaten
     setUser(state, payload) {     /*payload: wenn eingeloggt: ist das User-Objekt. Dieses 
                                     beiinhaltet alle Daten des users (Email, Passwort,etc.)
                                              wenn ausgeloggt: beträgt null*/
       state.user = payload        //Setzt den Benutzerzustand auf die übergebene Payload
       console.log('user state changed:', state.user)
     },
-    setAuthIsReady(state, payload) {
-      state.authIsReady = payload
+    setAuthIsReady(state, payload) {  /* in diesem Fall ist die payload true oder false
+                                      da es nur aufgerufen wird wenn die Authentifizierung bereit ist, ist es immer true*/
+      state.authIsReady = payload     
     }
   },
   actions: {
     async signup(context, { email, password }) { //Definition der Aktion "signup"
+                                                 /*context: Ist ein Objekt das verschieden Methoden
+                                                            und Eigenschaften enthält die benötigt werden*/
       console.log('signup action')
 
       // Versuche, einen Benutzer mit der bereitgestellten E-Mail und dem Passwort zu registrieren
@@ -57,16 +60,27 @@ const store = createStore({   //Hier wird der Authentifizierungsspeicher angeleg
     async logout(context) {
       console.log('logout action')
  
-      await signOut(auth)
-      context.commit('setUser', null)
+      await signOut(auth)               // logout Funktion (benötigt nur auth)
+      context.commit('setUser', null)   // Aktualisiere den Benutzer im Store mit null
     }
   }
 })
 
-// wait until auth is ready
+// Die Funktion onAuthStateChanged wird aufgerufen, um den Authentifizierungsstatus zu überwachen
+// Sie nimmt zwei Argumente entgegen: das Authentifizierungsobjekt (auth) und eine Callback-Funktion,
+// die aufgerufen wird, wenn sich der Authentifizierungsstatus ändert
 const unsub = onAuthStateChanged(auth, (user) => {
-  store.commit('setAuthIsReady', true)
+    /* Die Callback-Funktion erhält den aktuellen Benutzer (user) als Parameter, der entweder ein
+       Benutzerobjekt ist, wenn ein Benutzer angemeldet ist, oder null, wenn kein Benutzer angemeldet ist */
+
+  // Setze den Zustand 'authIsReady' im Vuex-Store auf true
+  store.commit('setAuthIsReady', true)   
+  
+  // Setze den Benutzer im Zustand des Vuex-Stores auf den aktuellen User
   store.commit('setUser', user)
+
+  //Beendet die Überwachung damit die Funktion nur einmal aufgerufen wird und nicht jedes mal
+  // wenn "auth" sich ändert
   unsub()
 })
 
